fd8b6442adfda97e13927bcd738816bdd6fed12f
unknown <adepuramya32@gmail.com>
2025-05-28 12:52:11 +0530

    deploy.sh file modified

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ SPLUNK_URL="https://8e56-136-232-205-158.ngrok-free.app"
 HEC_TOKEN="07a8d8e7-3e10-4f6e-b001-62cb662c962a"
 
 # Specify the log file and sourcetype directly here
-LOGFILE="logs/Webserver.log"         # ✅ Change this to your desired log file
-SOURCETYPE="wednesday"               # ✅ Change this to your desired sourcetype
+LOGFILE="logs/application.log"         # ✅ Change this to your desired log file
+SOURCETYPE="Ramya"               # ✅ Change this to your desired sourcetype
 INDEX="my_harness_index"           # ✅ Change this to your desired index
 
 # Debug info
@@ -31,4 +31,4 @@ else
   exit 1
 fi
 
-echo "✅ Deployment finished!!@@"
+echo "✅ Deployment finished!!!"
