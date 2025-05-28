80f4db01862dd3389fd3fde25f2880b6fdc47faf
adepuramya15 <162422718+adepuramya15@users.noreply.github.com>
2025-05-28 15:48:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SPLUNK_URL="https://8e56-136-232-205-158.ngrok-free.app"
 HEC_TOKEN="07a8d8e7-3e10-4f6e-b001-62cb662c962a"
 
 # Specify the log file and sourcetype directly here
-LOGFILE="logs/application.log"         # ✅ Change this to your desired log file
+LOGFILE="logs/app.log"         # ✅ Change this to your desired log file
 SOURCETYPE="Notepad"               # ✅ Change this to your desired sourcetype
 INDEX="my_harness_index"           # ✅ Change this to your desired index
 
