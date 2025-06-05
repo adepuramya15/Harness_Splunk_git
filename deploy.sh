282bbe41365364d07422633f87c49dcee96cec3c
unknown <adepuramya32@gmail.com>
2025-06-05 10:54:20 +0530

    5-06-2025

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
 # Hardcoded Splunk HEC values
-SPLUNK_URL="https://fafa-136-232-205-158.ngrok-free.app"
+SPLUNK_URL="https://ffb1-136-232-205-158.ngrok-free.app"
 HEC_TOKEN="07a8d8e7-3e10-4f6e-b001-62cb662c962a"
   
 # Specify the log file and source ltype directly here
 LOGFILE="logs/transaction.log"         # ✅ Change this to your desired log file
-SOURCETYPE="wed"               # ✅ Change this to your desired source type
+SOURCETYPE="itsthurs"               # ✅ Change this to your desired source type
 INDEX="my_harness_index"     # ✅ Change this to your desired index
  
 # Debug info
