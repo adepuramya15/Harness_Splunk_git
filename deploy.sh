ff04fe3866c17caf43c654392461c33c8bb294cc
adepuramya15 <162422718+adepuramya15@users.noreply.github.com>
2025-06-07 11:14:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
 # Hardcoded Splunk HEC values
-SPLUNK_URL="https://dfa3-136-232-205-158.ngrok-free.app"
+SPLUNK_URL="https://prd-p-xugh6.splunkcloud.com"
 HEC_TOKEN="9b8f0b3a-4e75-4f70-8689-8db4e22d73fb"
   
 # Specify the log file and source ltype directly here
 LOGFILE="logs/transaction.log"         # ✅ Change this to your desired log file
-SOURCETYPE="itsthurs"               # ✅ Change this to your desired source type
+SOURCETYPE="saturday"               # ✅ Change this to your desired source type
 INDEX="my_harness_index"     # ✅ Change this to your desired index
  
 # Debug info
@@ -20,7 +20,7 @@ if [[ -f "$LOGFILE" ]]; then
   echo "📤 Sending $LOGFILE to Splunk..."
   while IFS= read -r line; do
     curl --silent --output /dev/null \
-      -k "$SPLUNK_URL/services/collector" \
+      -k "$SPLUNK_URL:8088/services/collector" \
       -H "Authorization: Splunk $HEC_TOKEN" \
       -H "Content-Type: application/json" \
       -d "{\"event\": \"$line\", \"sourcetype\": \"$SOURCETYPE\", \"index\": \"$INDEX\"}" \
